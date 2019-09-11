7a372d9ba47df98108c0c8abefc429e3bd572f71
111 <wl02638153@gmail.com>
2019-09-11 14:23:23 +0800

    ELMAH

--- ShoppingCar/App_Start/RouteConfig.cs
+++ ShoppingCar/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace ShoppingCar
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces:new[] {"ShoppingCar.Controllers"}
             );
         }
     }
